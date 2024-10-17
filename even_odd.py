8da57fe03f05914ca133e3ec77b36fe831a2e1f8
Briankipkemboi47 <kipkemboi.brian@yahoo.com>
2024-10-17 08:01:09 +0300

    Initial Commit

---
+++ even_odd.py
@@ -0,0 +1,7 @@
+print("Welcome to Even Odd Checker!")
+value = int(input("Enter Your digit to check: \n"))
+
+if value % 2 == 0:
+    print("Your digit is even number.")
+else:
+    print("Your digit is an odd number.")
